7154ad3df27a9eb1b98e2243d1fd041fdc167446
root <root@localhost>
2017-12-31 14:05:48 +0100

    test

--- php/server.php
+++ php/server.php
@@ -1,3 +1,18 @@
 <?php
-
+if(isset($_GET['op'])) {
+    $op = $_GET['op];
+    switch($op){
+        case "list":
+            $data = "";
+            $fh = fopen("list.json", "r");
+            while ($line = fgets($fh)){
+                $data .= $line; 
+            } 
+            fclose($fh);
+            $data = json_decode($data);
+            header("Content-type:text/javascript"); 
+            echo json_encode($data);
+            break;
+    }
+}
 ?>
